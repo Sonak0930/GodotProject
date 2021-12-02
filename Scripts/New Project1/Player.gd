32c4d04cde105b624b4fae5d7a19b668700e001f
Sonak0930 <woals3850@gmail.com>
2021-12-02 21:36:43 +0900

    CreditScene
    Credit

--- Scripts/New Project1/Player.gd
+++ Scripts/New Project1/Player.gd
@@ -1,65 +1,26 @@
-extends Sprite
+extends KinematicBody2D
 
-class_name Player
-var angular_speed = PI
-
-var speed = 200
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-var health = 10
-
-
-signal damage_taken(old_value,new_value)
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var timer = get_node("Timer")
-	timer.connect("timeout",self,"_on_Timer_Timeout")
- # Replace with function body.
-
-func _on_Timer_Timeout():
-	visible = not visible
-	print("time out")
+export (int)var speed = 200
+export (float)var rotation_speed = 1.5
+var velocity = Vector2()
+var rotation_dir = 0
+func get_input():
 	
-func test():
-	print("test")
-func _circularMove(delta):
-	rotation += angular_speed * delta
-	var v = Vector2.UP.rotated(rotation) * speed
-	position += v * delta
+	look_at(get_global_mouse_position())
+	velocity = Vector2()
 	
-func _moveWithKeyboard():
-	var v1 = Vector2.ZERO
-	var v2 = Vector2.ZERO
-	if Input.is_action_pressed("ui_left"):
-		v1 = Vector2.LEFT
-	if Input.is_action_pressed("ui_right"):
-		v1 = Vector2.RIGHT
-	if Input.is_action_pressed("ui_up"):
-		v2 = Vector2.UP
+	
+	
 	if Input.is_action_pressed("ui_down"):
-		v2 = Vector2.DOWN
-		
-	v1 += v2
-	
-	position += v1
-	
-func _process(delta):
-	_moveWithKeyboard()
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+		velocity  = Vector2(-speed,0).rotated(rotation)
+	elif Input.is_action_pressed("ui_up"):
+		velocity  = Vector2(speed,0).rotated(rotation)
 
-func take_damage(amount):
-	var prevHealth= health;
-	health -= amount
-	
-	if health < 0:
-		print("Dead")
-		health = 0
-		
-	#emit signal to notify label.
-	emit_signal("damage_taken",prevHealth,health)
+#everything requires for physics need to be under here.
+func _physics_process(delta):
+	get_input()
+
+	#move
+	move_and_slide(velocity)
+
 	
