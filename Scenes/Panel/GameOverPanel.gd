2189f51055f309cf895a4dba2e5c24439b29d842
DongHyun96 <wadanka09@gmail.com>
2021-12-09 22:53:12 +0900

    merge commit

---
+++ Scenes/Panel/GameOverPanel.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var menu_scene = preload("res://Scenes/Menu.tscn")
+
+
+func _on_ExitGameButton_pressed():
+	#print("exit button pressed")
+	get_tree().paused = false
+	get_tree().change_scene_to(menu_scene)
+	
+
